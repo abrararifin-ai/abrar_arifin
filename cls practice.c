54431f50b47753f3df7e295f460134f82a95254d
abrar_arifin <abrararifin1234@gmail.com>
2026-06-20 02:38:59 +0600

    Add files via upload

---
+++ cls practice.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main ()
+
+{ int x ,y, sum;
+x=10;
+y=5;
+sum= x+y;
+printf("your number:,%d",sum);
+
+{int x,y, sum;
+x=10;
+y=5;
+sum=x*y;
+printf("your number:,%d",sum);
+
+ {
+   int x,y,sum;
+   x=20;
+   y=5;
+   sum=x/y;
+   printf("your number:,%d",sum);
+ }
+
+ {
+   int x,y,sum;
+   x=50;
+   y=10;
+   sum=x-y;
+   printf("your num:%d",sum);
+ }
+return 0; }
+
+
+}
+
